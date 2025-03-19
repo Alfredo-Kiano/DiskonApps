9dc4e19ddef9f5e3d1a64159fcf0909463da7e9e
Alfredo-Kiano <edhogcr@gmail.com>
2025-03-19 08:30:29 +0700

    Add files via upload

---
+++ SAATNYA CODING/JAva/script.js
@@ -0,0 +1,29 @@
+
+//OPERATOR LOGIKA (PRINT)
+/*
+let x = 780;
+let y = 808;
+console.log( x > 90 && y > x);
+console.log( x > 12 || y > x < 17 && !18>80 || !80<7);
+*/
+
+//MAIN IF DAN ELSE//
+
+let cekumur = 18;
+ if(cekumur >= 17) {
+    console.log('Sudah Termasuk!')
+ }
+ else{
+    console.log('Belum termasuk!')
+ }
+
+ let nilai = 80;
+
+ if(nilai >= 90){
+    console.log('COB')
+ }else if (nilai >= 70){
+    console.log("B")
+ }
+ else if( nilai >= 60) {
+    console.log("c")
+ }
